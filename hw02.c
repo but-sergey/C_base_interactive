d5e9439e215d7d01c6519af169d6e8772363ec93
but-sergey <but-sergey@bk.ru>
2020-06-03 08:58:43 +0300

    Добавил шапку-комментарий

--- hw02.c
+++ hw02.c
@@ -1,3 +1,12 @@
+/* Курс "Основы С. Интерактивный курс"
+ * Урок 2. Условия и циклы
+ * Практическая работа
+ *
+ * Выполнил Бут Сергей
+ * Среда Visual Studio Code v1.45.1
+ */
+
+
 #include <stdio.h>
 #include <locale.h>
 
@@ -14,15 +23,18 @@ int main (int argc, const char** argv)
     //         number, (number >= 0 && number <= 100) ? "" : "не ");
 
     // --2--
-    float avg = 0.0;
-    for (int i = 0; i < 10; i++)
-    {
-        printf("Введите целое число №%d: ", i);
-        scanf("%d", &number);
-        avg += number;
-    }
-    avg /= 10.0;
-    printf("Среднее арифметическое введенных чисел: %f", avg);
+    // float avg = 0.0;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("Введите целое число №%d: ", i);
+    //     scanf("%d", &number);
+    //     avg += number;
+    // }
+    // avg /= 10.0;
+    // printf("Среднее арифметическое введенных чисел: %f", avg);
+
+    // --3--
+
 
     return 0;
 }
