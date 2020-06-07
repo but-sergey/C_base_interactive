fffa4551fb8c36a7b15059211b8f5f1ee385df1e
but-sergey <but-sergey@bk.ru>
2020-06-07 17:01:18 +0300

    Задание 3 выполнено

--- hw04.c
+++ hw04.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <math.h>
 
 // --1--
 void decToBin(int num, char* result)
@@ -39,6 +40,27 @@ void calcRectangle(Rectangle* rect)
     rect->perimeter = 2 * (rect->height + rect->width);
 }
 
+// --3--
+struct point
+{
+    int x;
+    int y;
+};
+typedef struct point Point;
+
+struct line
+{
+    Point p1;
+    Point p2;
+    float length;
+};
+typedef struct line Line;
+
+void CalcLine(Line* line)
+{
+    line->length = sqrt(pow(line->p2.x - line->p1.x, 2) + pow(line->p2.y - line->p1.y, 2));
+}
+
 // =====================================
 int main(int argc, const char** argv)
 {
@@ -53,13 +75,29 @@ int main(int argc, const char** argv)
     // printf("„исло %d в двоичной системе равно %s", num, binary);
 
     // --2--
-    Rectangle rect;
-    printf("¬ведите высоту пр€моугольника: ");
-    scanf("%d", &rect.height);
-    printf("¬ведите ширину пр€моугольника: ");
-    scanf("%d", &rect.width);
-    calcRectangle(&rect);
-    printf("ѕериметр пр€моугольника равен %d, а площадь равна %d\n", rect.perimeter, rect.area);
+    // Rectangle rect;
+    // printf("¬ведите высоту пр€моугольника: ");
+    // scanf("%d", &rect.height);
+    // printf("¬ведите ширину пр€моугольника: ");
+    // scanf("%d", &rect.width);
+    // calcRectangle(&rect);
+    // printf("ѕериметр пр€моугольника равен %d, а площадь равна %d\n", rect.perimeter, rect.area);
+
+    // --3--
+    Line line;
+    printf("¬ведите координаты начала отрезка:\nx1 = ");
+    scanf("%d", &line.p1.x);
+    printf("y1 = ");
+    scanf("%d", &line.p1.y);
+    printf("¬ведите координаты конца отрезка:\nx2 = ");
+    scanf("%d", &line.p2.x);
+    printf("y2 = ");
+    scanf("%d", &line.p2.y);
+    
+    CalcLine(&line);
+
+    printf("ƒлина отрезка с началом в точке (%d; %d) и концом в точке (%d; %d) равна %f\n",
+            line.p1.x, line.p1.y, line.p2.x, line.p2.y, line.length);
 
     return 0;
 }
